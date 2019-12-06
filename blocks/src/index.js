5a5dc00e0346402941ebbdc8d90179bfb9066eaf
Lucio Negrello <lucionegrello@yahoo.com.ar>
2019-12-06 03:35:38 -0300

    Crear un bloque editable con JSX.

--- blocks/src/index.js
+++ blocks/src/index.js
@@ -5,7 +5,23 @@ registerBlockType('pgb/basic-block', {
 	description: 'Este bloque no tiene ninguna funcionalidad, simplemente es un Hello World.',
 	icon: 'smiley',
 	category: 'layout',
-	keywords: [ 'wordpress', 'gutenberg', 'platzigift'],
-	edit: () => <div>Hola, mundo!!!</div>,
-	save: () => <div>Hola, mundo!!!</div>,
+    keywords: [ 'wordpress', 'gutenberg', 'platzigift'],
+    attributes: {
+		content: {
+			type: 'string',
+			default: 'Hello world'
+		}
+	},
+	edit: (props) => {
+		const { attributes: { content }, setAttributes, className } = props;
+		const handlerOnChangeInput = (event) => {
+			setAttributes( { content: event.target.value } )
+		}
+		return <input
+					class={ className }
+					value={ content }
+					onChange={ handlerOnChangeInput }
+				/>
+	},
+	save: (props) => <h2 class={ props.className }>{ props.attributes.content }</h2>
 });
